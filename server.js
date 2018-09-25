b5ada60ae56daa05d5b4c5f9e9cb4011e200e0e5
Carlos Noel <carlosnoelg3@gmail.com>
2018-09-25 03:54:34 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'); //Paquete que se encuentra en el modulo de node
+const app = express();
+const hbs = require('hbs');
+
+// Helpers
+require('./hbs/helpers');
+
+// Puerto desde heroku
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    // res.send('Hola mundo!!!');
+
+    res.render('home', {
+        nombre: 'carlOs noeL'
+    });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('Hola mundo!!!');
+
+    res.render('about', {});
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola Data!!!');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port}`);
+});
